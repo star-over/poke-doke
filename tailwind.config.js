394f8b21d3c1ffd8a29bebeea4a988e3b974eb6f
FFire <8497715@gmail.com>
2023-04-14 00:37:15 +0500

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,45 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+export const content = [
+  './src/pages/**/*.{js,ts,jsx,tsx}',
+  './src/components/**/*.{js,ts,jsx,tsx}',
+  './src/app/**/*.{js,ts,jsx,tsx}',
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      poppins: ['var(--font-poppins)', ...fontFamily.sans],
     },
+    colors:{
+      primary: "#DC0A2D",
+      bug: {
+        50: '#fbfce9',
+        100: '#f7f8cf',
+        200: '#edf2a4',
+        300: '#dde76f',
+        400: '#cbd843',
+        500: '#a7b723',
+        600: '#879719',
+        700: '#667318',
+        800: '#525c18',
+        900: '#454e19',
+        950: '#242b08',
+      },
+      dark: {
+        50: '#f5f3f1',
+        100: '#e7e1da',
+        200: '#d1c3b7',
+        300: '#b6a08e',
+        400: '#a1826e',
+        500: '#927260',
+        600: '#75574c',
+        700: '#654943',
+        800: '#573f3c',
+        900: '#4c3837',
+        950: '#2b1d1d',
+    },
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
