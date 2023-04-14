9eb1a9b872b5372b2893054ce795c351d52e7aaf
FFire <8497715@gmail.com>
2023-04-15 00:33:37 +0500

    fix: linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,32 @@
 module.exports = {
   root: true,
-  env: { node: true },
+  env: {
+    es2022: true,
+    node: true,
+  },
   ignorePatterns: ["**/*.cjs", "next-env.d.ts", "*config.*", "package*.json", "*config.js", "./*.js"],
   extends: [
     'airbnb',
     'airbnb-typescript',
+    "next",
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    "plugin:array-func/all",
+    "plugin:eslint-comments/recommended",
     "plugin:react/jsx-runtime",
     "plugin:tailwindcss/recommended",
-    "plugin:import/recommended",
-    "plugin:json/recommended",
+    "plugin:import/typescript",
+    "plugin:json/recommended-with-comments",
     "plugin:no-use-extend-native/recommended",
     "plugin:pii/recommended",
     "plugin:sonarjs/recommended",
     "plugin:switch-case/recommended",
     "plugin:unicorn/all",
-    "plugin:xss/recommended"
+    "plugin:xss/recommended",
+    "plugin:markdown/recommended",
+    "plugin:no-unsanitized/DOM",
+    "plugin:promise/recommended",
+    "plugin:security/recommended",
     // "auto"
   ],
   plugins: [
@@ -28,28 +38,36 @@ module.exports = {
     "markdown",
     "optimize-regex",
     "promise",
-    "simple-import-sort",
     "sonarjs",
     "switch-case",
-    "unicorn",
     "no-constructor-bind",
     "no-secrets",
     "no-unsanitized",
     "pii",
     "security",
     "react-hooks",
-    "jsx-a11y",
-    "xss"
+    "unicorn",
+    "xss",
+    "tailwindcss"
+  ],
+  overrides: [
+    {
+        files: ["**/*.md"],
+        processor: "markdown/markdown"
+    },
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    // extraFileExtensions: [".json", ".md"],
+    extraFileExtensions: [".json", ".md"],
     ecmaVersion: "latest",
     sourceType: 'module',
     parser: "@typescript-eslint/parser",
     project: "./tsconfig.eslint.json",
   },
   settings: {
+    "html/html-extensions": [".html"],
+    "html/xml-extensions": [".html"],
+    "mdx/code-blocks": true,
     "import/resolver": {
       typescript: { "alwaysTryTypes": true }
     }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable tailwindcss/no-custom-classname */
-export default function Home(_ff:boolean) {
+export default function Home() {
   return (
+    // eslint-disable-next-line tailwindcss/no-custom-classname
     <h2 className="font-poppins text-ice mb-3 text-2xl font-semibold">
       Poké Doké&nbsp;
 
